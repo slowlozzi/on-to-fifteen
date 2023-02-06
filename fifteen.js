5a4c7fff2ce5c3d1dfc1af9c803405b966f558e1
slowlozzi <misters2003@gmail.com>
2023-02-06 14:34:17 +0200

    Create fifteen.js

---
+++ fifteen.js
@@ -0,0 +1,16 @@
+ let i = 0;
+  while (i <= 50) {
+    
+    i++
+ console.log(i)
+};
+
+
+
+function Nums () {
+ let i = 0;
+  while (i <= 50) {
+    
+    i++
+  } return i;
+}console.log(Nums ());
